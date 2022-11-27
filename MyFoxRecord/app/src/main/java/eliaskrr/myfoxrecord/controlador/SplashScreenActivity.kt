ee1dc1548cfb3471f5ebdf9b5ba0130019e320b4
DESKTOP-U5O35HJ\Usuario <ilyasseessadak@gmail.com>
2022-11-27 22:19:11 +0100

    Cambiando el logo de la app

---
+++ MyFoxRecord/app/src/main/java/eliaskrr/myfoxrecord/controlador/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package eliaskrr.myfoxrecord.controlador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eliaskrr.myfoxrecord.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
