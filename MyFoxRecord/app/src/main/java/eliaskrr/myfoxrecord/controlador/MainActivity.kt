aa343f23ddf0b17050399f858397d6d0b6194845
DESKTOP-U5O35HJ\Usuario <ilyasseessadak@gmail.com>
2022-11-27 21:24:34 +0100

    Se borra todo lo anterior porque se va a cambiar de diseño

--- MyFoxRecord/app/src/main/java/eliaskrr/myfoxrecord/controlador/MainActivity.kt
+++ MyFoxRecord/app/src/main/java/eliaskrr/myfoxrecord/controlador/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import eliaskrr.myfoxrecord.R
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
