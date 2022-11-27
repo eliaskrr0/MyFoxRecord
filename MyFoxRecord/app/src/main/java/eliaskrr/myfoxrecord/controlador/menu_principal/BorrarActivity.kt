08ad03b31feec009bc4fc3e841c8af2cc7c4b8f6
DESKTOP-U5O35HJ\Usuario <ilyasseessadak@gmail.com>
2022-11-27 21:15:49 +0100

    ·Agregando los botones del menú principal y organizando el proyecto

---
+++ MyFoxRecord/app/src/main/java/eliaskrr/myfoxrecord/controlador/menu_principal/BorrarActivity.kt
@@ -0,0 +1,12 @@
+package eliaskrr.myfoxrecord.controlador.menu_principal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eliaskrr.myfoxrecord.R
+
+class BorrarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_borrar)
+    }
+}
--- MyFoxRecord/app/src/main/java/eliaskrr/myfoxrecord/controlador/menu_principal/MostrarActivity.kt
+++ MyFoxRecord/app/src/main/java/eliaskrr/myfoxrecord/controlador/menu_principal/MostrarActivity.kt
@@ -1,11 +1,12 @@
-package eliaskrr.myfoxrecord
+package eliaskrr.myfoxrecord.controlador.menu_principal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import eliaskrr.myfoxrecord.R
 
-class MainActivity : AppCompatActivity() {
+class MostrarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_mostrar)
     }
 }
